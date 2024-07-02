3c5f0b89053614cd3b2b00cd4a7b0782058c166a
dyf96 <yingfei.dai@atrae.co.jp>
2024-07-02 14:20:39 +0900

    ✨ feat: setup commit format

---
+++ .cz-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  types: [
+    { value: "✨ feat", name: "✨ feat:     A new feature" },
+    { value: "🐛 fix", name: "🐛 fix:      A bug fix" },
+    { value: "📝 docs", name: "📝 docs:     Documentation only changes" },
+    {
+      value: "💄 style",
+      name: "💄 style:    Changes that do not affect the meaning of the code",
+    },
+    {
+      value: "♻️ refactor",
+      name: "♻️ refactor: A code change that neither fixes a bug nor adds a feature",
+    },
+    {
+      value: "⚡️ perf",
+      name: "⚡️ perf:     A code change that improves performance",
+    },
+    {
+      value: "✅ test",
+      name: "✅ test:     Adding missing tests or correcting existing tests",
+    },
+    {
+      value: "📦️ build",
+      name: "📦️ build:    Changes that affect the build system or external dependencies",
+    },
+    {
+      value: "👷 ci",
+      name: "👷 ci:       Changes to our CI configuration files and scripts",
+    },
+    {
+      value: "🔧 chore",
+      name: "🔧 chore:    Other changes that don't modify src or test files",
+    },
+    { value: "⏪️ revert", name: "⏪️ revert:   Reverts a previous commit" },
+  ],
+  skipQuestions: ["scope", "body", "breaking", "footer"],
+  subjectLimit: 100,
+};
